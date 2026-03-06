c05b13642f6f7a3a169fd9f43174f2d3cb6d0910
csar paez.12 <dangergipsy635@gmail.com>
2026-03-06 16:18:06 -0300

    WEB python

---
+++ web/script.js
@@ -0,0 +1,8 @@
+ botonTraducir = document.getElementById('bTraducir');
+
+botonTraducir.addEventListener('click',
+    function(){
+        titulo = document.getElementById('titulo');
+        titulo.textContent = "Register form";
+    }
+);
